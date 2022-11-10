c35208e5c4ee84e893609b9fbf3fc13399603f79
Ricardo Fernández <ricfdzgon@gmail.com>
2022-11-10 15:56:16 +0100

    Contador huevos

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var botonHuevos = document.getElementById("boton_huevo");
 var contadorHuevos = 0;
 
+const botonQuitar = document.createElement("button");
+botonQuitar.type = "button";
+botonQuitar.innerText = "Quitar huevos";
+
 botonHuevos.addEventListener("click", function (event) {
   contadorHuevos++;
+  document.getElementById("parrafo2").innerHTML =
+    "Huevos comidos: " + contadorHuevos;
+  document.body.appendChild(botonQuitar);
+});
+
+botonQuitar.addEventListener("click", function (event) {
+  if (contadorHuevos > 0) {
+    contadorHuevos--;
+  }
   document.getElementById("parrafo2").innerHTML =
     "Huevos comidos: " + contadorHuevos;
 });
