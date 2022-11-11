0840dc47d590ea51aaa4d06d6bee2c2919ca9ccb
Ricardo Fernández <ricfdzgon@gmail.com>
2022-11-11 14:10:54 +0100

    Lógica campo password

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 var botonHuevos = document.getElementById("boton_huevo");
 var contadorHuevos = 0;
+var canEdit = true;
 
 const botonQuitar = document.createElement("button");
 botonQuitar.type = "button";
-//botonQuitar.id = "botonQuitar";
-//botonQuitar.innerText = "";
+
+const cajaPassWord = document.createElement("input");
+cajaPassWord.type = "password";
+
+const comprobarPass = document.createElement("button");
+botonQuitar.type = "button";
 
 botonHuevos.addEventListener("click", function (event) {
-  contadorHuevos++;
-  document.getElementById("parrafo2").innerHTML =
-    "Huevos comidos: " + contadorHuevos + "\t";
-  document.getElementById("parrafo2").appendChild(botonQuitar);
+  if (canEdit) {
+    contadorHuevos++;
+    document.getElementById("parrafo2").innerHTML =
+      "Huevos comidos: " + contadorHuevos + "\t";
+    document.getElementById("parrafo2").appendChild(botonQuitar);
+    document.getElementById("parrafoBotonQuitar").appendChild(cajaPassWord);
+    document.getElementById("parrafoBotonQuitar").appendChild(comprobarPass);
+  }
 });
 
 botonQuitar.addEventListener("click", function (event) {
-  if (contadorHuevos > 0) {
-    contadorHuevos--;
+  if (canEdit) {
+    if (contadorHuevos > 0) {
+      contadorHuevos--;
+    }
+    document.getElementById("parrafo2").innerHTML =
+      "Huevos comidos: " + contadorHuevos + "\t";
+    if (contadorHuevos > 0) {
+      document.getElementById("parrafo2").appendChild(botonQuitar);
+    } else {
+      document.getElementById("parrafoBotonQuitar").removeChild(cajaPassWord);
+    }
   }
-  document.getElementById("parrafo2").innerHTML =
-    "Huevos comidos: " + contadorHuevos + "\t";
-  document.getElementById("parrafo2").appendChild(botonQuitar);
 });
