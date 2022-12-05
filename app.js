5ad3086ad358809ffe5a585d4db4d4465219b143
Ricardo Fernández <ricfdzgon@gmail.com>
2022-12-05 13:32:17 +0100

    Implementación sidebar index

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+// sidebar toggle
+const btnToggle = document.querySelector(".toggle-btn");
+
+btnToggle.addEventListener("click", function () {
+  console.log("clik");
+  document.getElementById("sidebar").classList.toggle("active");
+  console.log(document.getElementById("sidebar"));
+});
+
 var botonHuevos = document.getElementById("boton_huevo");
 var contadorHuevos = 0;
 var canEdit = true;
